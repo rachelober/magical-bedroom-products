51ff988830cb9182230e1c587204b088d91945aa
Rachel Ober <rachelober@gmail.com>
2016-08-09 05:49:50 -0400

    Items can be removed from cart now

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -5,3 +5,18 @@ $(document).ready(function() {
       $(".cart__count").text(data.length);
     });
 });
+
+$(".item__remove").click(function() {
+  var item = $(this);
+  var data_id = item.data("id");
+  var parent_label = "cart-item-" + data_id;
+  var api = "http://localhost:3000/cart_order/" + data_id;
+
+  $.ajax({
+    url: api,
+    type: 'DELETE',
+    success: function(result) {
+      $("#cart-item-" + data_id).remove();
+    }
+  });
+});
